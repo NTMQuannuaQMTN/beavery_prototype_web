fd97264c534f0f00726047839b6558bb8b509a2a
nguyentruongmanhquan@gmail.com <nguyentruongmanhquan@gmail.com>
2025-11-25 11:02:07 +0700

    Làm tạm be auth

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
+import authRouter from "./routes/auth.js";
 
 const app = express();
 const port = 3000;
+
+app.use('/auth', authRouter);
 
 app.use(cors());
 app.listen(port, () => {
